cf14be5136a1d4f645e9a689e1c5adeefe5915bd
Jason Gardner <n01480000@unf.edu>
2022-12-05 09:29:12 -0500

    Home Page

--- Models/IShelterRepository.cs
+++ Models/IShelterRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenBed.Models
 {
@@ -7,5 +9,7 @@ namespace OpenBed.Models
     {
         IEnumerable<Shelter> Shelters { get; }
         public virtual IEnumerable<Shelter> GetShelters() => Shelters;
+        public virtual Shelter GetShelter(Guid id) => Shelters.FirstOrDefault(s => s.ShelterID == id);
+        public virtual Boolean ContainsID(Guid id) => Shelters.Any(s => s.ShelterID == id);
     }
 }
