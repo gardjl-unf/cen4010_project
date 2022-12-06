162c3eb00945b72774e54412121152b00d6c7804
Jason Gardner <n01480000@unf.edu>
2022-12-06 16:13:13 -0500

    Retrieve Shelter for Administration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OpenBed.Models;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+using System.Net.Http;
+using System.Security.Claims;
 
 namespace OpenBed.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        IShelterRepository repo;
+        private Shelter shelter;
+        public AccountController (IShelterRepository iShelterRepository)
         {
-            return View();
+            repo = iShelterRepository;
+            shelter = repo.GetShelter(Guid.Parse(this.User.FindFirst(ClaimTypes.NameIdentifier).Value));
         }
+        [Authorize]
+        public ViewResult Index()
+        {
+            return View(shelter);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Index(Shelter shelter) 
+        { 
+            return View(shelter); 
+        }
+
     }
 }
--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
+using System.Threading.Tasks;
 
 namespace OpenBed.Models
 {
     public class AppUser : IdentityUser<Guid>
     {
-
-
+        
     }
 }
